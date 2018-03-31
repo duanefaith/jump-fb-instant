9c80346fdce7a5de00dc1e328d9c285d72905ab2
duanefaith <duanedu@sarahyanzi.synology.me>
2018-03-31 10:56:30 +0800

    --first commit

---
+++ src/assets/Entry.js
@@ -0,0 +1,1 @@
+window.shared={},window.shared.getFBInstant=function(){return window.hasOwnProperty("FBInstant")?FBInstant:null},window.shared.testOptions={},window.shared.testOptions.roomHost="wss://server.jumpo.xyz:3000",window.shared.getOptions=function(){return window.shared.testOptions},cc.director.getPhysicsManager().enabled=!0;
